ca54e280677202496f1ace3c7099296ed8b55c41
Charleston Anjos <charleston.anjos@gmail.com>
2018-12-27 15:21:39 -0200

    add annotation @Parcelize

--- application/domain/src/main/java/br/com/charleston/domain/model/GithubModel.kt
+++ application/domain/src/main/java/br/com/charleston/domain/model/GithubModel.kt
@@ -1,7 +1,12 @@
 package br.com.charleston.domain.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class GithubModel(
     val name: String,
     val fullName: String,
+    val private: String,
     val user: UserModel
-)
+) : Parcelable
--- application/domain/src/main/java/br/com/charleston/domain/model/UserModel.kt
+++ application/domain/src/main/java/br/com/charleston/domain/model/UserModel.kt
@@ -1,6 +1,10 @@
 package br.com.charleston.domain.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class UserModel(
     val avatarUrl: String,
     val name: String
-)
+) : Parcelable
